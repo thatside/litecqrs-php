1d40f93959f6d9d330af7add79755b3324ccb3ee
thatside <bturchik@zaraffasoft.com>
2017-10-23 16:30:32 +0300

    Updates JMSSerializerPass to depend on JMS serializer and not on Symfony serializer

--- src/LiteCQRS/Plugin/SymfonyBundle/DependencyInjection/Compiler/JMSSerializerPass.php
+++ src/LiteCQRS/Plugin/SymfonyBundle/DependencyInjection/Compiler/JMSSerializerPass.php
@@ -10,7 +10,7 @@ class JMSSerializerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ( ! $container->has('serializer')) {
+        if ( ! $container->has('jms_serializer')) {
             return;
         }
 
